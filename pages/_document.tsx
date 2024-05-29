dae16a2593efecd3c31a59a4f382f3bd2a1b62d7
Slendy_Milky <slendymilky@gmail.com>
2024-05-30 00:04:37 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,83 +1,62 @@
-import * as React from 'react'
-import Document, { Head, Html, Main, NextScript } from 'next/document'
-
-import { IconContext } from 'react-icons'
+import * as React from 'react';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { IconContext } from 'react-icons';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
-        <Html lang='en'>
+        <Html lang="en">
           <Head>
-            <link rel='shortcut icon' href='/favicon.ico' />
-            <link
-              rel='icon'
-              type='image/png'
-              sizes='32x32'
-              href='favicon-32x32.png'
-              />
-              <link
-                rel='apple-touch-icon'
-                sizes='180x180'
-                href='/apple-touch-icon.png'
-              />
-              <link
-                rel='icon'
-                type='image/png'
-                sizes='96x96'
-                href='/favicon-96x96.png'
-              />
-
-            <link rel='manifest' href='/manifest.json' />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+            <link rel="manifest" href="/manifest.json" />
           </Head>
-
           <body>
             <script
               dangerouslySetInnerHTML={{
                 __html: `
-/** Inlined version of noflash.js from use-dark-mode */
-;(function () {
-  var storageKey = 'darkMode'
-  var classNameDark = 'dark-mode'
-  var classNameLight = 'light-mode'
+(function () {
+  var storageKey = 'darkMode';
+  var classNameDark = 'dark-mode';
+  var classNameLight = 'light-mode';
+
   function setClassOnDocumentBody(darkMode) {
-    document.body.classList.add(darkMode ? classNameDark : classNameLight)
-    document.body.classList.remove(darkMode ? classNameLight : classNameDark)
+    document.body.classList.toggle(classNameDark, darkMode);
+    document.body.classList.toggle(classNameLight, !darkMode);
   }
-  var preferDarkQuery = '(prefers-color-scheme: dark)'
-  var mql = window.matchMedia(preferDarkQuery)
-  var supportsColorSchemeQuery = mql.media === preferDarkQuery
-  var localStorageTheme = null
+
+  var mql = window.matchMedia('(prefers-color-scheme: dark)');
+  var supportsColorSchemeQuery = mql.media === '(prefers-color-scheme: dark)';
+  var localStorageTheme;
+
   try {
-    localStorageTheme = localStorage.getItem(storageKey)
+    localStorageTheme = localStorage.getItem(storageKey);
+    if (localStorageTheme !== null) {
+      localStorageTheme = JSON.parse(localStorageTheme);
+    }
   } catch (err) {}
-  var localStorageExists = localStorageTheme !== null
-  if (localStorageExists) {
-    localStorageTheme = JSON.parse(localStorageTheme)
-  }
-  // Determine the source of truth
-  if (localStorageExists) {
-    // source of truth from localStorage
-    setClassOnDocumentBody(localStorageTheme)
+
+  if (localStorageTheme !== null) {
+    setClassOnDocumentBody(localStorageTheme);
   } else if (supportsColorSchemeQuery) {
-    // source of truth from system
-    setClassOnDocumentBody(mql.matches)
-    localStorage.setItem(storageKey, mql.matches)
+    setClassOnDocumentBody(mql.matches);
+    localStorage.setItem(storageKey, mql.matches);
   } else {
-    // source of truth from document.body
-    var isDarkMode = document.body.classList.contains(classNameDark)
-    localStorage.setItem(storageKey, JSON.stringify(isDarkMode))
+    var isDarkMode = document.body.classList.contains(classNameDark);
+    localStorage.setItem(storageKey, JSON.stringify(isDarkMode));
   }
 })();
-`
+                `,
               }}
             />
             <Main />
-
             <NextScript />
           </body>
         </Html>
       </IconContext.Provider>
-    )
+    );
   }
 }
