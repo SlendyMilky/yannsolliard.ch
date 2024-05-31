a230b81028fef761001d23a9287e96e6d60db474
Slendy_Milky <slendymilky@gmail.com>
2024-05-31 12:58:13 +0200

    quelques modifs

--- next.config.js
+++ next.config.js
@@ -14,7 +14,6 @@ module.exports = withBundleAnalyzer({
       'pbs.twimg.com',
       'abs.twimg.com',
       's3.us-west-2.amazonaws.com',
-      'blog.snackga.me',
       'yannsolliard.ch',
       'docs.slyc.ch'
 
